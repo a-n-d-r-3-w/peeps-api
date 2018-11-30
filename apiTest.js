5f40f8265e974c3084e2119f797427e4aa4de85d
Andrew <andrew.liu01@libertymutual.com>
2018-11-30 15:52:23 -0500

    Failing test: Check that account has been added

--- apiTest.js
+++ apiTest.js
@@ -3,7 +3,8 @@ const HttpStatus = require('http-status-codes')
 
 const request = supertest('http://localhost:3000')
 
-request.get('/accounts').expect(HttpStatus.OK, { accounts: [] }, function (error) {
+// Get all accounts (which is an empty array at this point)
+request.get('/accounts').expect(HttpStatus.OK, { accounts: [] }, error => {
   if (error) {
     console.error(error)
     return
@@ -11,7 +12,17 @@ request.get('/accounts').expect(HttpStatus.OK, { accounts: [] }, function (error
   console.info('pass')
 })
 
+// Create an account
 request.post('/accounts').expect(HttpStatus.CREATED, { accountId: 1 }, error => {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.info('pass')
+})
+
+// Get all accounts again
+request.get('/accounts').expect(HttpStatus.OK, { accounts: [ 1 ] }, error => {
   if (error) {
     console.error(error)
     return
