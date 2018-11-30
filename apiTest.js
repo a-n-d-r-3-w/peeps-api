01330f47724de81984523bec61c85d7618fc268e
Andrew <andrew.liu01@libertymutual.com>
2018-11-30 15:33:42 -0500

    First failing test

---
+++ apiTest.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest')
+const HttpStatus = require('http-status-codes')
+
+const request = supertest('http://localhost:3000')
+
+request.get('/accounts').expect(HttpStatus.OK, function (error) {
+  if (error) {
+    console.error(error)
+    return
+  }
+  console.info('pass')
+})
